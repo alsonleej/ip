0e6e6a8e8e2d86fd33e64e55207108d5a31c8ab1
alsonleej <e1398387@u.nus.edu>
2025-08-19 00:02:15 +0800

    Level 0: Basic setup with Kip chatbot

---
+++ src/main/java/Kip.java
@@ -0,0 +1,11 @@
+public class Kip {
+    public static void main(String[] args) {
+        String logo = "____________________________________________________________\n"
+                + "Hello! I'm Kip\n"
+                + "What can I do for you?\n"
+                + "____________________________________________________________\n"
+                + "Bye. Hope to see you again soon!\n"
+                + "____________________________________________________________\n";
+        System.out.println(logo);
+    }
+}
