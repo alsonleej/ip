788233156ae87a7f00ce4c64f57f35af1b7002fe
alsonleej <e1398387@u.nus.edu>
2025-08-19 00:09:11 +0800

    Echo implemented

--- src/main/java/Kip.java
+++ src/main/java/Kip.java
@@ -1,11 +1,30 @@
+import java.util.Scanner;
+
 public class Kip {
     public static void main(String[] args) {
-        String logo = "____________________________________________________________\n"
+        String greeting = "____________________________________________________________\n"
                 + "Hello! I'm Kip\n"
                 + "What can I do for you?\n"
-                + "____________________________________________________________\n"
-                + "Bye. Hope to see you again soon!\n"
                 + "____________________________________________________________\n";
-        System.out.println(logo);
+        
+        System.out.println(greeting);
+        
+        Scanner scanner = new Scanner(System.in);
+        String userInput;
+        
+        // Command loop
+        while (true) {
+            
+            userInput = scanner.nextLine().trim();
+            
+            if (userInput.equalsIgnoreCase("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            } else {
+                System.out.println(userInput);
+            }
+        }
+        
+        scanner.close();
     }
 }
