c310ce7fd62bfd21a64d38d96030106c696c41d7
alsonleej <e1398387@u.nus.edu>
2025-08-19 00:24:50 +0800

    Add list functionality

--- src/main/java/Kip.java
+++ src/main/java/Kip.java
@@ -1,27 +1,47 @@
 import java.util.Scanner;
 
 public class Kip {
-    public static void main(String[] args) {
-        String greeting = "____________________________________________________________\n"
-                + "Hello! I'm Kip\n"
-                + "What can I do for you?\n"
+    private static final int MAX_INPUTS = 100;
+
+    private static void output(String text) {
+        String output = "____________________________________________________________\n"
+                + text + "\n"
                 + "____________________________________________________________\n";
-        
-        System.out.println(greeting);
+        System.out.println(output);
+    }
+
+    public static void main(String[] args) {
+        output("Hello! I'm Kip\nWhat can I do for you?\n");
         
         Scanner scanner = new Scanner(System.in);
         String userInput;
+
+        String[] userInputs = new String[MAX_INPUTS];
+        int inputCount = 0;
         
-        // Command loop
         while (true) {
-            
             userInput = scanner.nextLine().trim();
             
             if (userInput.equalsIgnoreCase("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
+                output("Bye. Hope to see you again soon!");
                 break;
+            } else if (userInput.equalsIgnoreCase("list")) {
+                // Show all stored inputs
+
+                String out = "";
+                for (int i = 0; i < inputCount; i++) {
+                    out += (i + 1) + ". " + userInputs[i] + "\n";
+                }
+                output(out);
             } else {
-                System.out.println(userInput);
+                // Store the input in the array if there's space
+                if (inputCount < MAX_INPUTS) {
+                    userInputs[inputCount] = userInput;
+                    inputCount++;
+                    output("added:" + userInput);
+                } else {
+                    output("Array is full! Cannot store more inputs.");
+                }
             }
         }
         
